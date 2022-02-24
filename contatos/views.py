5f467722005bc582c148feb95d61c8c660acf07e
Elias Rodrigues <eliasantoniorodrigues1@gmail.com>
2022-02-23 22:06:01 -0300

    Inserção de paginação, edição e configuração da tela de admin

---
+++ contatos/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from .models import Contato
+
+
+def index(request):
+    contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 2)
+    
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+
+
+def ver_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+    })
